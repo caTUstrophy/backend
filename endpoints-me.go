ff5b5e4cde6ea3c1c411cfbaa1c76cf50b4e826b
= <thrash.meil@gmx.de>
2016-06-15 21:28:37 +0200

    added GET /me

--- endpoints-me.go
+++ endpoints-me.go
@@ -11,10 +11,41 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+var fieldsGetUser = map[string]interface{}{
+	"Name":"Name",
+	"PreferredName":"PreferredName",
+	"Mail":"Mail",
+	"MailVerified":"MailVerified",
+	"Groups":map[string]interface{}{
+		"Permissions":map[string]interface{}{
+			"AccessRight":"AccessRight",
+		},
+	},
+}
+
+var fieldsListUserOffers = map[string]interface{}{
+	"Name":"Name",
+	"Location":"Location",
+	"Tags":map[string]interface{}{
+		"Name":"Name",
+	},
+}
+
+var fieldsListUserRequests = map[string]interface{}{
+	"Name":"Name",
+	"Location":"Location",
+	"Tags":map[string]interface{}{
+		"Name":"Name",
+	},
+}
+
+
+
+
 func (app *App) GetUser(c *gin.Context) {
 
 	// Check authorization for this function.
-	ok, _, message := app.Authorize(c.Request)
+	ok, User, message := app.Authorize(c.Request)
 	if !ok {
 
 		// Signal client an error and expect authorization.
@@ -24,32 +55,10 @@ func (app *App) GetUser(c *gin.Context) {
 		return
 	}
 
-	// TODO: Change stub to real function.
-	c.JSON(http.StatusOK, gin.H{
-		"ID":            fmt.Sprintf("%s", uuid.NewV4()),
-		"Name":          "Bernd",
-		"PreferredName": "Da Börnd",
-		"Mail":          "esistdermomentgekommen@mail.com",
-		"Groups": struct {
-			Location    interface{}
-			Permissions interface{}
-		}{
-			struct {
-				Longitude float64
-				Latitude  float64
-			}{
-				13.5,
-				50.2,
-			},
-			struct {
-				AccessRight string
-				Description string
-			}{
-				"user",
-				"This permission represents a standard, registered but not privileged user in our system.",
-			},
-		},
-	})
+	var obj db.User
+	app.DB.Preload("Groups").Preload("Groups.Permissions").First(&obj, "id = ?", User.ID)
+	response := db.CopyNestedModel(obj, fieldsGetUser)
+	c.JSON(http.StatusOK, response)
 }
 
 func (app *App) UpdateUser(c *gin.Context) {
@@ -112,20 +121,11 @@ func (app *App) ListUserOffers(c *gin.Context) {
 
 	response := make([]interface{}, 0)
 	for _, o := range Offers {
-
 		// 2) Check expired field - extra argument for that?
 		if o.ValidityPeriod.After(time.Now()) {
 
 			// 3) Only return what's needed
-			fields := map[string]interface{}{
-				"Name":"Name",
-				"Location":"Location",
-				"Tags":map[string]interface{}{
-					"Name":"Name",
-				},
-			}
-
-			model := db.CopyNestedModel(o, fields)
+			model := db.CopyNestedModel(o, fieldsListUserOffers)
 			response = append(response, model)
 		}
 	}
@@ -150,23 +150,12 @@ func (app *App) ListUserRequests(c *gin.Context) {
 	var Requests []db.Request
 	app.DB.Preload("Tags").Find(&Requests, "user_id = ?", User.ID)
 
-
 	response := make([]interface{}, 0)
 	for _, r := range Requests {
-
 		// 2) Check expired field - extra argument for that?
 		if r.ValidityPeriod.After(time.Now()) {
-
 			// 3) Only return what's needed
-			fields := map[string]interface{}{
-				"Name":"Name",
-				"Location":"Location",
-				"Tags":map[string]interface{}{
-					"Name":"Name",
-				},
-			}
-
-			model := db.CopyNestedModel(r, fields)
+			model := db.CopyNestedModel(r, fieldsListUserRequests)
 			response = append(response, model)
 		}
 	}
