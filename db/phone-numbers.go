5ebe46a630722eaab96e23cddfc16b491b88dd9c
Lennart Oldenburg <hello@lennartoldenburg.de>
2016-06-24 17:23:31 +0200

    Fixes the panic / error on reading phone numbers from database. Sorry, my mistake. Closes caTUstrophy/backend#76 and caTUstrophy/backend#77.

--- db/phone-numbers.go
+++ db/phone-numbers.go
@@ -4,7 +4,9 @@
 package db
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 	"strings"
 
 	"database/sql/driver"
@@ -15,23 +17,34 @@ type PhoneNumbers []string
 
 func (num *PhoneNumbers) Scan(value interface{}) error {
 
-	// Start of JSON string to be unmarshalled later.
-	jsonByteString := "[ "
+	var err error
 
-	// Make sure we operate on a string slice.
-	numbers := value.([]string)
+	if reflect.TypeOf(value).String() == "[]string" {
 
-	// Range over supplied numbers and append each to JSON string.
-	for _, n := range numbers {
-		jsonByteString = fmt.Sprintf("%s\"%s\", ", jsonByteString, n)
+		// Start of JSON string to be unmarshalled later.
+		jsonByteString := "[ "
+
+		// Make sure we operate on a string slice.
+		numbers := value.([]string)
+
+		// Range over supplied numbers and append each to JSON string.
+		for _, n := range numbers {
+			jsonByteString = fmt.Sprintf("%s\"%s\", ", jsonByteString, n)
+		}
+
+		// Remove trailing comma and space and replace by closing bracket of JSON string.
+		jsonByteString = strings.TrimRight(jsonByteString, ", ")
+		jsonByteString = fmt.Sprintf("%s ]", jsonByteString)
+
+		err = json.Unmarshal([]byte(jsonByteString), &num)
+	} else if reflect.TypeOf(value).String() == "[]uint8" {
+		err = json.Unmarshal(value.([]byte), &num)
+	} else {
+		return errors.New("Could not scan phone number - type assertion failed.")
 	}
 
-	// Remove trailing comma and space and replace by closing bracket of JSON string.
-	jsonByteString = strings.TrimRight(jsonByteString, ", ")
-	jsonByteString = fmt.Sprintf("%s ]", jsonByteString)
-
 	// Attempt to unmarshal into type.
-	if err := json.Unmarshal([]byte(jsonByteString), &num); err != nil {
+	if err != nil {
 		return err
 	}
 
