6d0fa3ea27191cfa04d37cc412346b02ac636a40
= <thrash.meil@gmx.de>
2016-06-26 22:32:04 +0200

    added GET /groups

---
+++ endpoints-groups.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+
+	"github.com/caTUstrophy/backend/db"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func (app *App) GetGroups(c *gin.Context) {
+
+	// Check authorization for this function.
+	ok, User, message := app.Authorize(c.Request)
+	if !ok {
+
+		// Signal client an error and expect authorization.
+		c.Header("WWW-Authenticate", fmt.Sprintf("Bearer realm=\"CaTUstrophy\", error=\"invalid_token\", error_description=\"%s\"", message))
+		c.Status(http.StatusUnauthorized)
+
+		return
+	}
+
+	// Check if user permissions are sufficient (user is admin).
+	if ok = app.CheckScope(User, db.Region{}, "superadmin"); !ok {
+		// Signal client that the provided authorization was not sufficient.
+		c.Header("WWW-Authenticate", "Bearer realm=\"CaTUstrophy\", error=\"authentication_failed\", error_description=\"Could not authenticate the request\"")
+		c.Status(http.StatusUnauthorized)
+
+		return
+	}
+
+
+	var Groups []db.Group
+	app.DB.Preload("Users").Find(&Groups)
+
+	models := make([]map[string]interface{}, len(Groups))
+	// Iterate over all groups in database return and marshal it.
+	for i, group := range Groups {
+		// TODO : resolve region in smarter way
+		if group.RegionId != "" {
+			var Region db.Region
+			app.DB.First(&Region, "id = ?", group.RegionId)
+			group.Region = Region
+		}
+
+		models[i] = CopyNestedModel(group, fieldsGroup).(map[string]interface{})
+	}
+
+	c.JSON(http.StatusOK, models)
+}
+
+
+func (app *App) ListSystemAdmins(c *gin.Context) {
+
+	// Check authorization for this function.
+	ok, User, message := app.Authorize(c.Request)
+	if !ok {
+
+		// Signal client an error and expect authorization.
+		c.Header("WWW-Authenticate", fmt.Sprintf("Bearer realm=\"CaTUstrophy\", error=\"invalid_token\", error_description=\"%s\"", message))
+		c.Status(http.StatusUnauthorized)
+
+		return
+	}
+
+	// Check if user permissions are sufficient (user is admin).
+	if ok := app.CheckScope(User, db.Region{}, "superadmin"); !ok {
+
+		// Signal client that the provided authorization was not sufficient.
+		c.Header("WWW-Authenticate", "Bearer realm=\"CaTUstrophy\", error=\"authentication_failed\", error_description=\"Could not authenticate the request\"")
+		c.Status(http.StatusUnauthorized)
+
+		return
+	}
+
+	// The real request
+
+	var group db.Group
+	app.DB.Preload("Users").First(&group, "access_right = ?", "superadmin")
+
+	model := CopyNestedModel(group.Users, fieldsSystemAdmin)
+
+	c.JSON(http.StatusOK, model)
+}
